0c1e188c14d0be8bad0011e116785a99bc542a50
bjostad <bjostad@gmail.com>
2018-07-06 13:29:26 -0700

    Delete spotifyConfig.js

--- spotifyConfig.js
+++
@@ -1,8 +0,0 @@
-/**
- * Spotify Config Info - This must be edited with a valid ID, Secret and Redirect URI
- */
-module.exports = {
-    CLIENT_ID: 'e3f3bf892ce3295ab294e8f6743da839',
-    CLIENT_SECRET: 'ba12b9cb4f7c2b76ac67c19ed900',
-    REDIRECT_URI: 'http://localhost:3000/callback'
-};
